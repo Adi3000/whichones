759823c969b89a345562b5d64b4c6ba073532bcb
adi3000 <adi3000@free.fr>
2013-12-30 17:37:33 +0100

    Map some entity (not complete)

--- src/main/java/net/whichones/common/database/data/Group.java
+++ src/main/java/net/whichones/common/database/data/Group.java
@@ -2,8 +2,5 @@ package net.whichones.common.database.data;
 
 public class Group {
 
-	public Group() {
-		// TODO Auto-generated constructor stub
-	}
-
+	
 }
--- src/main/java/net/whichones/common/database/data/Section.java
+++ src/main/java/net/whichones/common/database/data/Section.java
@@ -1,9 +1,51 @@
 package net.whichones.common.database.data;
 
-public class Section {
+import com.adi3000.common.database.hibernate.data.AbstractDataObject;
 
-	public Section() {
-		// TODO Auto-generated constructor stub
+public class Section extends AbstractDataObject{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2549731896898570265L;
+	private String name;
+	private Integer id;
+	private Integer index;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
 	}
-
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the index
+	 */
+	public Integer getIndex() {
+		return index;
+	}
+	/**
+	 * @param index the index to set
+	 */
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
 }
